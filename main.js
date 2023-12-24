8371eeb45aea7a6c1d4f022203c774c5ad5ec3b6
Rana-Parthvi-298285 <134841182+Rana-Parthvi-298285@users.noreply.github.com>
2023-12-24 14:09:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var x= document.getElementById("myAudio");
 
 function new_image()
 {
-fabric.Image.fromURL('BirthdayImage.jpg', function (Img))
+fabric.Image.fromURL('BirthdayImage.jpg', function (Img) {
 block_image_object = Img;       
 block_image_object.scaleToWidth(700);
 block_image_object.scaleToWidth(510);
@@ -11,11 +11,11 @@ block_image_object.set({
 top:0,
 left:0
 });
-canvas.add(block_image);    	
-
+canvas.add(block_image); 
+});
 
 }
 
 function playSound(){
-x.play();
+myAudio.play();
 }
